08d1f655c801ecbad975ed92264dd2c9a54aff2a
gooodforeal <2390603@gmail.com>
2023-04-07 00:54:42 +0300

    ver 1.0.0

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="TRPP_project",
+    version="0.0.1",
+    author="IKBO-08-21",
+    author_email="2390603@gmail.com",
+    description="Navigator bot for telegram",
+    url="https://github.com/gooodforeal/TRPP_project",
+    packages=['TRPP_project'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5'
+)
